2af351eb1fd324682774a827dcc831648a508b8a
Theo <quangthang99@gmail.com>
2022-03-09 19:39:44 +0700

    fix and adjust test case for Home

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,42 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/pages/index'
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+import { Provider } from "react-redux";
+import { store } from "store";
 
-describe('Home', () => {
-  it('renders a heading', () => {
-    render(<Home />)
+describe("Home", () => {
+  it("Should render headings for menu and cart", () => {
+    render(
+      <Provider store={store}>
+        <Home products={[]} />
+      </Provider>
+    );
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
+    const menuHeading = screen.getByText("Menu");
+    const cartHeading = screen.getByText("Cart");
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(menuHeading).toBeInTheDocument();
+    expect(cartHeading).toBeInTheDocument();
+  });
+
+  it("Should render given product information", () => {
+    render(
+      <Provider store={store}>
+        <Home
+          products={[
+            {
+              id: 1,
+              name: "Small Pizza",
+              description: "lorem ipsum",
+              price: 11.99,
+              image: "",
+            },
+          ]}
+        />
+      </Provider>
+    );
+
+    const product = screen.getByText("Small Pizza");
+
+    expect(product).toBeInTheDocument();
+  });
+});
